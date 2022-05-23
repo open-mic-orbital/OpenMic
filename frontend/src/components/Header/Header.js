a44a874d998512e07be8dc9a8296223ab108014d
kxrt <kvrtikeya@gmail.com>
2022-05-23 19:26:26 +0800

    Fixed formatting

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,17 +1,20 @@
 import "./Header.css";
-import openMicLogo from '../../utils/images/OpenMicLogo.png';
+import openMicLogo from "../../utils/images/OpenMicLogo.png";
 
 function Header() {
-    return (
-        <header>
-            <div className="Header" style={{ display: "flex", flexFlow: "row nowrap" }}>
-                    <img src={openMicLogo} height={50} alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                </div>
-        </header>
-    );
+  return (
+    <header>
+      <div
+        className="Header"
+        style={{ display: "flex", flexFlow: "row nowrap" }}
+      >
+        <img src={openMicLogo} height={50} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
