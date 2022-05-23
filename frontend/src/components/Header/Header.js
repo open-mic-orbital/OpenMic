e6f577220709af615f11c70480b86612eadef300
kxrt <kvrtikeya@gmail.com>
2022-05-23 19:24:48 +0800

    Implemented header design

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,19 +1,15 @@
-// import "../../App.css";
-import "./Header.module.css";
-
+import "./Header.css";
 import openMicLogo from '../../utils/images/OpenMicLogo.png';
 
 function Header() {
     return (
         <header>
-            <div className="Header.module">
-                <div style={{ display: "flex", flexFlow: "row nowrap" }}>
-                    <img src={openMicLogo} className="Header.module-logo" alt="logo" />
+            <div className="Header" style={{ display: "flex", flexFlow: "row nowrap" }}>
+                    <img src={openMicLogo} height={50} alt="logo" />
                     <p>
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
                 </div>
-            </div>
         </header>
     );
 }
