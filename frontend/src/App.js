95b0e70c03f92432cf76c033eecb8ad9e6c69d90
nramapurath <nramapurath@gmail.com>
2022-05-24 11:34:17 +0800

    Added MUI icons manually to fix peer dependency conflict, tested with AppBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import ResponsiveAppBar from "./components/ResponsiveAppBar/ResponsiveAppBar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ResponsiveAppBar />
     </div>
   );
 }
