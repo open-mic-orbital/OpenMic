b13418a13f50061aa56e70b85cb8806471b1ec6e
kxrt <kvrtikeya@gmail.com>
2022-05-25 16:35:45 +0800

    initialised react-router, implemented barebones Discover page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,24 +10,16 @@ import ForArtistsButton from "./components/ForEachButton/ForArtistsButton";
 import ForVenuesButton from "./components/ForEachButton/ForVenuesButton";
 import Stack from "@mui/material/Stack";
 // import Container from "@mui/material/Container";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* Below came with Box, is white background, with centered content.
-      Can switch to this to see borders against the white background later on if needed*/
-      /* <Box
-        sx={{
-          display: 'flex',
-          flexDirection: { xs: 'column', md: 'row' },
-          alignItems: 'center',
-          bgcolor: 'background.paper',
-          overflow: 'hidden',
-          borderRadius: '12px',
-          boxShadow: 1,
-          fontWeight: 'bold',
-        }}
-      > */}
       <Box sx={{ flexGrow: 1, color: "white", overflow: "scroll" }}>
         <Grid container spacing={2}>
           <Grid item xs={12} sx={{ paddingBottom: "5vh" }}>
@@ -50,3 +42,19 @@ function App() {
 }
 
 export default App;
+
+
+/* Below came with Box, is white background, with centered content. Goes below classname="App" if necessary.
+      Can switch to this to see borders against the white background later on if needed */
+      /* <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' },
+          alignItems: 'center',
+          bgcolor: 'background.paper',
+          overflow: 'hidden',
+          borderRadius: '12px',
+          boxShadow: 1,
+          fontWeight: 'bold',
+        }}
+      > */
