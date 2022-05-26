0512a6d9a508c5baf6dd04563c02dee1edb0ec7d
nramapurath <nramapurath@gmail.com>
2022-05-26 16:22:40 +0800

    Added password hashing pre-save, and login route

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,8 +1,9 @@
 // Define user model
 
 const mongoose = require('mongoose');
-const validator = require('validator') // to validate email
-const jwt = require('jsonwebtoken')
+const validator = require('validator'); // to validate email
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -36,22 +37,47 @@ const userSchema = new mongoose.Schema({
         type: String,
         enum: ['artist', 'venue'],
         required: true
-    }
-    // tokens: [{
-    //     token: {
-    //         type: String,
-    //         required: true
-    //     }
-    // }]
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
-// userSchema.methods.generateAuthToken = async function () {
-//     const user = this;
-//     const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
-//     user.tokens = user.tokens.concat({ token });
-//     await user.save();
-//     return token;
-// }
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new Error('Unable to login');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password); // to compare if password hashes match
+
+    if (!isMatch) {
+        throw new Error('Unable to login');
+    }
+
+    return user;
+}
+
+// Hash plaintext password before saving
+userSchema.pre('save', async function (next) { // needs to be non-arrow function to use 'this'
+    const user = this;
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next(); // to confirm done with pre ops to move on to save
+})
 
 const User = mongoose.model('User', userSchema);
 
--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -17,11 +17,21 @@ router.post('/users', async (req, res) => {
     }
 });
 
+router.post('/users/login', async (req, res) => {
+    try{
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({ user, token });
+    } catch (e) {
+        res.status(400).send();
+    }
+})
+
 // Read ALL users (remove later, currently only for dev purposes)
 router.get('/users', async (req, res) => {
     try {
         const users = await User.find({});
-        
+
         if (!users) { // No users found
             return res.status(404).send();
         }
