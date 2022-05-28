91e4730716eee70e98c6b24c3841a9e99caf6b4d
kxrt <kvrtikeya@gmail.com>
2022-05-28 15:09:05 +0800

    Cleaned up App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,4 @@
 import "./App.css";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-// import Header from "./components/Header/Header";
-// import AuthAppBar from "./components/AuthAppBar/AuthAppBar";
-import UnauthAppBar from "./components/UnauthAppBar/UnauthAppBar";
-import MainText from "./components/MainText/MainText";
-import Icons from "./utils/images/Icons.png";
-import ForArtistsButton from "./components/ForEachButton/ForArtistsButton";
-import ForVenuesButton from "./components/ForEachButton/ForVenuesButton";
-import Stack from "@mui/material/Stack";
-// import Container from "@mui/material/Container";
 import {
   BrowserRouter as Router,
   Routes,
@@ -40,18 +29,3 @@ function App() {
 }
 
 export default App;
-
-/* Below came with Box, is white background, with centered content. Goes below classname="App" if necessary.
-      Can switch to this to see borders against the white background later on if needed */
-/* <Box
-        sx={{
-          display: 'flex',
-          flexDirection: { xs: 'column', md: 'row' },
-          alignItems: 'center',
-          bgcolor: 'background.paper',
-          overflow: 'hidden',
-          borderRadius: '12px',
-          boxShadow: 1,
-          fontWeight: 'bold',
-        }}
-      > */
